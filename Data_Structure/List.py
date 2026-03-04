ad1ae4124becfabe93de7a2007cd4c49ddcc5d74
killlercam <herosviral@gmail.com>
2026-03-04 19:13:47 +0530

    Some codes

---
+++ Data_Structure/List.py
@@ -0,0 +1,19 @@
+# lists are ordered, mutable collections of items.
+# They can contain items of different data types
+
+#Type checking
+lst = []
+print(type(lst))
+
+#Adding Elements
+names = ["Shashwat", "Palak", 1, 2, 4]
+print(names)
+
+
+## Accessing List Elements
+#In python Indexes starts with 1 but here we will start with one
+Fruits = ["apple", "banana", "cherry", "Kiwi", "guava"]
+print(Fruits[1]) #banana
+print(Fruits[-1]) #guava
+print(Fruits[1:6])
+print(Fruits[-1::])
